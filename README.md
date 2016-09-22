94395bc2652126cf437bdc0192799c495ec7143f
BananaAcid <repo@bananaacid.de>
2016-09-22 08:12:41 +0200

    initial

---
+++ README.md
@@ -0,0 +1,58 @@
+# node-red-sysmessage
+
+This module provides a node for Node-RED to quickly show a message at the host system (OSX, Windows).
+
+It is distributed on NPM [node-red-sysmessage](https://www.npmjs.com/package/node-red-sysmessage).
+
+## Pre-requisites
+
+This requires Node-RED version 0.14 or more recent.
+
+## Install
+
+Run the following command in your Node-RED user directory (typically `~/.node-red`):
+
+```
+npm install node-red-sysmessage
+```
+
+Open your Node-RED instance and you should have the node available in the palette and a new `system` tab in right side panel.
+
+#### Features
+
+It supports OSX and Windows:
+- OSX Alert
+	- A basic OS message box
+- OSX Notification
+	- Right hand notification banner, will also be added to the notification center
+- OSX Message / iMessage
+	- sends messages through the messages.app to any buddy in your list, including iMessages (Hint: add yourself to send to)
+- WIN Alert
+	- A basic OS message box, at least Win 7
+- WIN Notification (will not be added to the notification center)
+	- requires POWERSHELL to be available - means at least Win 8 (or earlier with powershell manually installed)
+	- does not support a receiver.
+
+#### Note
+
+It is completely based on CLI commands.
+The message field supports mustache, using the msg fields.
+If no receiver is set, it uses the current host system (not a remote) to show the message.
+
+
+#### Node Setup
+
+1 in: the payload
+1 out [optional]: any console error or message created
+
+
+#### Example flow
+
+`Button -> alert -> debug out`
+
+
+Import using the Menu->Import->Clipboard.
+
+```
+[{"id":"72013ad3.c27184","type":"sysmessage","z":"2fc3038e.7502e4","command":"osxalert","title":"tit","subtitle":"st","op1":"asd 'fsdf\\n123","op1type":"str","receiver":"","name":"","x":247,"y":260,"wires":[["8c391e4.bfe5be"]]},{"id":"8c391e4.bfe5be","type":"debug","z":"2fc3038e.7502e4","name":"Show debug result","active":true,"console":"true","complete":"payload","x":403,"y":334,"wires":[]},{"id":"c6834579.0edbc","type":"inject","z":"2fc3038e.7502e4","name":"","topic":"","payload":"Button: test it.","payloadType":"str","repeat":"","crontab":"","once":false,"x":154,"y":173,"wires":[["72013ad3.c27184"]]}]
+```
---
+++ nodes/sysmessage.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright(c) 2016 Nabil Redmann <repo@nabil-redmann.de>
+ * MIT Licensed
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var exec = require('child_process').exec;
+    var isUtf8 = require('is-utf8');
+    var mustache = require("mustache");
+
+    function SysMessageNode(n) {
+        RED.nodes.createNode(this,n);
+        this.cmd = (n.command || "").trim();
+        this.title = (n.title || "").trim();
+        this.subtitle = (n.subtitle || "").trim();
+        this.receiver = (n.receiver || "").trim();
+        this.timer = Number(n.timer || 0)*1000;
+        this.activeProcesses = {};
+
+
+        this.op1 = n.op1 || "";
+        this.op1type = n.op1type || "str";
+        if (this.op1type === 'val') {
+            if (this.op1 === 'true' || this.op1 === 'false') {
+                this.op1type = 'bool'
+            } else if (this.op1 === 'null') {
+                this.op1type = 'null';
+                this.op1 = null;
+            } else {
+                this.op1type = 'str';
+            }
+        }
+        var node = this;
+
+
+        var cleanup = function(p) {
+            //console.log("CLEANUP!!!",p);
+            node.activeProcesses[p].kill();
+            node.status({fill: "red", shape:"dot", text:"timeout"});
+            node.error("Exec node timeout");
+        }
+
+        this.on("input", function(msg) {
+            var child;
+            node.status({fill:"blue", shape:"dot", text:" "});
+           
+                let payload = null;
+                
+                if (msg && node.op1type === 'pay') { // && msg.hasOwnProperty("payload")) {
+                    payload = mustache.render(msg.payload, node).replace('\n', '\\n');
+                }
+                else if (msg && this.op1type === 'str') {
+                    payload = mustache.render(node.op1, node).replace('\n', '\\n');;
+                }
+
+                var cl = '';
+
+                switch (node.cmd) {
+                    case 'osxnotification': {
+                        let modPayload = payload.replace(/"/g, "´").replace(/'/g, "´");
+                        let modTitle = node.title.replace(/"/g, "´").replace(/'/g, "´");
+                        let modSubtitle = node.subtitle.replace(/"/g, "´").replace(/'/g, "´");
+
+                        if (node.receiver.length) {
+                            cl += `tell application "Finder" of machine "eppc://${node.receiver}" to `;
+                        }
+
+                        cl += `display notification "${modPayload}" with title "${modTitle}" subtitle "${modSubtitle}"`;
+                        cl  = `osascript -e '${cl}'`;
+
+                        break;
+                    }
+
+                    case 'osxalert': {
+                        let modPayload = payload.replace(/"/g, "´").replace(/'/g, "´");
+                        let modTitle = node.title.replace(/"/g, "´").replace(/'/g, "´");
+                        let modSubtitle = node.subtitle.replace(/"/g, "´").replace(/'/g, "´");
+
+                        if (node.receiver.length) {
+                            cl += `of machine "eppc://${node.receiver}" `;
+                        }
+
+                        cl  = `tell application "System Events" ${cl} to display dialog "${modSubtitle}\\n\\n${modPayload}" with title "${modTitle}" buttons "ok"`;
+                        cl  = `osascript -e '${cl}'`;
+
+                        break;
+                    }
+
+                    case 'osxmessage': {
+                        let modPayload = payload.replace(/"/g, "´").replace(/'/g, "´");
+                        let modTitle = node.title + (node.subtitle ? ': ' + node.subtitle : '');
+                        modTitle = modTitle.replace(/"/g, "´").replace(/'/g, "´");
+
+                        let modMessage = `${modTitle}\n\n${modPayload}`;
+
+                        cl += `tell application "Messages" to send "${modMessage}" to buddy "${node.receiver}"`;
+                        cl  = `osascript -e '${cl}'`;
+                        break;
+                    }
+
+
+
+                    case 'winalert': {
+                        let receiver = node.receiver || process.env.USERNAME; //'%username%';
+                        let modPayload = payload.replace(/\\n/g, ' ').replace(/"/g, "'");
+                        let modTitle = node.title.replace(/\\n/g, ' ').replace(/"/g, "'");
+                        let modSubtitle = node.subtitle.replace(/\\n/g, ' ').replace(/"/g, "'");
+
+                        cl = `msg "${receiver}" "${modTitle} - ${modSubtitle}     ${modPayload}"`;
+                        break;
+                    }
+
+                    case 'winnotification': {
+                        let modTitle = node.title + (node.subtitle ? ' - ' + node.subtitle : '');
+                        modTitle = modTitle.replace(/'/g, "`");
+                        let modPayload = payload.replace(/\\n/g, '\n').replace(/'/g, "`");
+                        let c = Buffer.from(`. '${__dirname}\\parts\\_windows-notificationicon.ps1'; Show-BalloonTip -Title '${modTitle}' -Message '${modPayload}';`, 'utf16le').toString('base64');
+
+                        cl = `PowerShell -EncodedCommand ${c}`;
+                    }
+                }
+
+                ///console.log( require('util').inspect( ['p', this.op1, this.op1type, process.cwd(), __dirname] ) );
+                ///console.log( require('util').inspect( ['CL', cl] ) );
+
+                /* istanbul ignore else  */
+                if (RED.settings.verbose) { node.log(cl); }
+                child = exec(cl, {encoding: 'binary', maxBuffer:10000000}, function (error, stdout, stderr) {
+
+                    ///console.log( require('util').inspect( [error, stdout, stderr] ) );
+                    
+                    var msg = {};
+
+                    msg.payload = new Buffer(stdout || stderr, "binary").toString('utf8');
+                    if (isUtf8(msg.payload)) { msg.payload = msg.payload.toString(); }
+                    if (error !== null) {
+                        msg.payload = error;
+                    }
+
+                    node.status({});
+                    node.send([msg]);
+                    if (child.tout) { clearTimeout(child.tout); }
+                    delete node.activeProcesses[child.pid];
+                });
+                child.on('error',function() {});
+                if (node.timer !== 0) {
+                    child.tout = setTimeout(function() { cleanup(child.pid); }, node.timer);
+                }
+                node.activeProcesses[child.pid] = child;
+            
+        });
+        this.on('close',function() {
+            for (var pid in node.activeProcesses) {
+                /* istanbul ignore else  */
+                if (node.activeProcesses.hasOwnProperty(pid)) {
+                    if (node.activeProcesses[pid].tout) { clearTimeout(node.activeProcesses[pid].tout); }
+                    node.activeProcesses[pid].kill();
+                }
+            }
+            node.activeProcesses = {};
+            node.status({});
+        });
+    }
+    RED.nodes.registerType("sysmessage",SysMessageNode);
+}
